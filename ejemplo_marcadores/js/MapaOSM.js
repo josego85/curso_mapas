68736a5f063a66ef61041c8625b111b13c67a529
proyectosbeta <josego85@gmail.com>
2016-05-14 11:52:14 -0400

    Se agrega la tercera parte del curso. Se muestra usar marcadores y que muestre informacion

---
+++ ejemplo_marcadores/js/MapaOSM.js
@@ -0,0 +1,19 @@
+// Variables y Objetos globales.
+var mapa = null;
+
+function cargarMapa(){
+	// Asuncion - Paraguay.
+	var longitud = -57.6309129;
+	var latitud = -25.2961407;
+	var zoom = 14;
+
+    // Se instancia el objeto mapa.
+	mapa =  L.map('map-container').setView([latitud, longitud], zoom);
+
+	// Humanitarian Style.
+	L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright">' +
+          'OpenStreetMap Contributors </a> Tiles \u00a9 HOT'
+	}).addTo(mapa);
+}
